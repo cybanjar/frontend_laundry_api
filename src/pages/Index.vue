ee5d4e7dd321e3c7164114af4148c6a473af2c62
cybanjar <cybanjar@gmail.com>
2021-03-01 22:03:10 +0700

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+    
+    <q-btn unelevated color="primary" label="Save" />
+    <p>{{ name }}</p>
+  </q-page>
+</template>
+
+
+<script>
+import { reactive, toRefs, onMounted } from "@vue/composition-api";
+
+export default {
+  name: 'PageIndex',
+
+  data() {
+    return {
+      name: 'Ahmad'
+    }
+  },
+
+  mounted() {
+    console.log('Mounted!');
+  }
+
+  // setup() {
+  //   const state = reactive({
+  //     name: 'Amin'
+  //   });
+
+  //   onMounted(() => {
+  //     console.log('Mounted!');
+  //   })
+
+  //   return {
+  //     ...toRefs(state),
+  //   }
+  // }
+}
+</script>
